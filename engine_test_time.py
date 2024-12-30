4c93a1d721e8a0b422860538294ca0e58dd6d31d
Paulcauch <paulo.caucheteux@gmail.com>
2024-12-30 18:38:10 +0100

    bug

--- engine_test_time.py
+++ engine_test_time.py
@@ -149,11 +149,11 @@ def train_on_test(base_model: torch.nn.Module,
                 original = samples[0].clone()  
                 patch_size = 16
                 masked_image = apply_mask_to_image(original, mask[0], patch_size)
-                print(pred_patches[0].unsqueeze(0))
+                print(pred_patches[0].unsqueeze(0).shape)
                 reconstructed_img = model.unpatchify(pred_patches[0].unsqueeze(0))                 
                 mask1 = mask[0].clone()
-                mask1[mask == 1] = 0
-                mask1[mask == 0] = 1
+                mask1[mask[0] == 1] = 0
+                mask1[mask[0] == 0] = 1
                 reconstructed_img = apply_mask_to_image(reconstructed_img, mask1, patch_size)
                 display_images(samples,masked_image,reconstructed_img)
 
